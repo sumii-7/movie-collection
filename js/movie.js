84bfeefd74f3ac59349d3550d564817b937436b6
수미 iPad <sumipad@sumiui-MacBookAir.local>
2024-04-25 20:40:20 +0900

    movie.js 수정

--- js/movie.js
+++ js/movie.js
@@ -12,27 +12,42 @@ fetch(
   options
 )
   .then((response) => response.json())
-  .then((data) => {
-    const movies = data.results; // 영화 목록 추출
+  .then((response) => {
+    const movies = response.results; // 영화 목록 추출
 
-    const moviesArea = document.getElementById("card"); // 영화를 추가할 html 영역 선택
+    // html의 id 값 가져오기
+    const moviesArea = document.getElementById("card");
 
-    let html = ""; // html 템플릿 문자열 초기화
-
-    movies.forEach((movie) => {
-      html += `
+    function movieList(val = "") {
+      moviesArea.innerHTML = "";
+      const res = movies
+        .map((movie) => {
+          if (movie.title.includes(val)) {
+            return `
     <div class="movie_card" onclick="showAlert(${movie.id})">
-    <div class="image_box"><img class="image" src="https://image.tmdb.org/t/p/w500${movie.poster_path}"></div>
+        <div class="image_box"><img class="image" src="https://image.tmdb.org/t/p/w500${movie.poster_path}"></div>
         <h3>${movie.title}</h3>
         <p>${movie.overview}</p>
         <p>Rating: ${movie.vote_average}</p>
     </div>
-    `; // html 템플릿 추가
+    `;
+          } // html 템플릿 추가
+        })
+        .join("");
+      moviesArea.innerHTML = res;
+    }
+    movieList();
+
+    // 검색기능 구현을 위한 html id 가져오기
+    const searchInput = document.getElementById("movie_name"); // input
+    const searchBtn = document.getElementById("searchbtn"); // button
+
+    searchBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      const val = searchInput.value;
+      console.log(val);
+      movieList(val);
     });
-
-    moviesArea.innerHTML = html; // html 영역에 html 붙이기
-
-    const movie_search = document.getElementById("movie_name").value;
   })
   .catch((err) => console.error(err));
 
